07eb8368a1dd1d508ada2f5b3605cf4dc4e62e8a
Jim Rosenblum <jrosenblum@carelogistics.com>
2016-05-25 13:52:48 -0400

    First commit

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
-;;;;
-;; Packages
-;;;;
+ (setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.8.1/emacs"
+      load-path))
+      (setq erlang-root-dir "/usr/local/lib/erlang")
+      (setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+      (require 'erlang-start)
+
+(add-to-list 'exec-path "/usr/local/bin")
+(require 'erlang-flymake)
+
+
 
 ;; Define package repositories
 (require 'package)
@@ -9,23 +16,64 @@
 (add-to-list 'package-archives
              '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
+             '("melpa" . "https://melpa.org/packages/") t)
 
 
-;; Load and activate emacs packages. Do this first so that the
-;; packages are loaded before you start trying to modify them.
-;; This also sets the load path.
 (package-initialize)
 
-;; Download the ELPA archive description if needed.
-;; This informs Emacs about the latest versions of all packages, and
-;; makes them available for download.
 (when (not package-archive-contents)
   (package-refresh-contents))
+
+
+
+(show-paren-mode t)
+(column-number-mode t)
+
+;(load-theme 'hc-zenburn t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("5ee12d8250b0952deefc88814cf0672327d7ee70b16344372db9460e9a0e3ffc" "9e54a6ac0051987b4296e9276eecc5dfb67fdcd620191ee553f40a9b6d943e78" "beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e87a2bd5abc8448f8676365692e908b709b93f2d3869c42a4371223aab7d9cf8" "4217c670c803e8a831797ccf51c7e6f3a9e102cb9345e3662cc449f4c194ed7d" "4eaad15465961fd26ef9eef3bee2f630a71d8a4b5b0a588dc851135302f69b16" default)))
+ '(fci-rule-color "#2a2a2a"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;(global-set-key (kbd "C-h") 'menu-bar-open)
+;(require 'airline-themes)
+;(load-theme 'airline-light)
+;(load-theme 'tomorrow-night-bright)
+
+
+;(defun windmove-emacs-or-tmux(dir tmux-cmd)
+;  (interactive)
+;  (if (ignore-errors (funcall (intern (concat "windmove-" dir))))
+;      nil                       ;; Moving within emacs
+;    (shell-command tmux-cmd)) ;; At edges, send command to tmux
+ ; ;
+;
+;					;Move between windows with custom keybindings
+;(global-set-key (kbd "M-P")
+;		'(lambda () (interactive) (windmove-emacs-or-tmux "up"  "tmux select-pane -U")))
+;(global-set-key (kbd "M-N")
+;		'(lambda () (interactive) (windmove-emacs-or-tmux "down"  "tmux select-pane -D")))
+;(global-set-key (kbd "M-F")
+;		'(lambda () (interactive) (windmove-emacs-or-tmux "right" "tmux next-window")))
+;(global-set-key (kbd "M-B")
+;		'(lambda () (interactive) (windmove-emacs-or-tmux "left"  "tmux previous-window")))
+;
+
 
 ;; The packages you want installed. You can also install these
 ;; manually with M-x package-install
@@ -33,7 +81,7 @@
 (defvar my-packages
   '(;; makes handling lisp expressions much, much easier
     ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-    paredit
+    ;;paredit
 
     ;; key bindings and code colorization for Clojure
     ;; https://github.com/clojure-emacs/clojure-mode
@@ -60,7 +108,7 @@
     projectile
 
     ;; colorful parenthesis matching
-    rainbow-delimiters
+    ;rainbow-delimiters-mode
 
     ;; edit html tags like sexps
     tagedit
@@ -68,21 +116,13 @@
     ;; git integration
     magit))
 
-;; On OS X, an Emacs instance started from the graphical user
-;; interface will have a different environment than a shell in a
-;; terminal window, because OS X does not run a shell during the
-;; login. Obviously this will lead to unexpected results when
-;; calling external utilities like make from Emacs.
-;; This library works around this problem by copying important
-;; environment variables from the user's shell.
-;; https://github.com/purcell/exec-path-from-shell
 (if (eq system-type 'darwin)
     (add-to-list 'my-packages 'exec-path-from-shell))
+
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
 
 ;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
 ;; to load them.
